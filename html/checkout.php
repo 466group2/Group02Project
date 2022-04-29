f7fa60205bfe7ed763b8a8b137ec3721aa0b1ae1
Moses Mang <z1913099@turing.cs.niu.edu>
2022-04-28 19:46:28 -0500

    Address and Submit button

--- html/checkout.php
+++ html/checkout.php
@@ -3,6 +3,14 @@ declare(strict_types = 1);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
+include '../lib/db.php';
+include '../lib/functions.php';
+
+if(isset($_POST["home"]))
+{
+    header("location:home.php");
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,13 +20,38 @@ error_reporting(E_ALL);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Checkout</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        .home{
+            position: absolute;
+            top: 10px;
+            left: 10px;
+            font-size: 25px;
+            margin: 20px 10px;
+        }
+        .submit{
+            position: relative;
+            font-size: 25px;
+            margin: 20px 10px;
+        }
+        div {
+            margin-bottom: 10px;
+        }
+        label{
+            display: inline-block;
+            width: 100px;
+        }
+    </style>
 </head>
 <body>
     <h1>Checkout</h1>    
     <h2>Please review your final order request:</h2>
+    <form method="POST">
+        <button class="button home" name="home">Home 
+            <i class="fa fa-home"></i>
+        </button>
+    </form>
 <?php
-include 'secrets.php';
-include 'library.php';
 
 //Connect to mariadb
 try {
@@ -29,5 +62,38 @@ try {
 catch(PDOexception $error){  
     die('    <p> Connection to database failed: ' . $error->getMessage() . "</p>\n </body></html>"); 
 } ?>
+
+<h3>Payment</h3>
+<form method="POST">
+    <div>
+        <label>Credit Card:</label>
+        <input type="text" id="CREDIT_CARD"/>
+    </div>
+</form>
+
+<h3>Shipping Address</h3>
+<form method="POST">
+    <div>
+        <label>Street:</label>
+        <input type="text" id="STREET_NAME"/>
+    </div>
+    <div>
+        <label>City:</label>
+        <input type="text" id="CITY"/>
+    </div>
+    <div>
+        <label>State:</label>
+        <input type="text" id="STATE"/>
+    </div>
+    <div>
+        <label>ZIP Code:</label>
+        <input type="text" id="ZIP"/>
+    </div>
+    <button class="button submit" name="submit">
+        Submit <i class="fa fa-check"></i>
+    </button>
+    
+</form>
+
 </body>
 </html>
