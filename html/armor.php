64ebc1f84b071543096c13efedae2a3beb8d374c
Alexander Peterson <z1912480@students.niu.edu>
2022-04-29 15:30:43 -0500

    add home and cart button to armor and item, remove old home button

--- html/armor.php
+++ html/armor.php
@@ -23,7 +23,8 @@ if(isset($_POST["home"]))
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Armors</title>
     <link rel="stylesheet" href="style.css">
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
     <style>   
         body {
         height: 100%;
@@ -69,17 +70,7 @@ if(isset($_POST["home"]))
     </style>
 </head>
 
-<?php
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Armors</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
+
 <body>
     <h1>Armors</h1>
     <h2>Keeping yourself lead-free</h2>
