8bd44119b126fecefc2cee5cbbde5d86dcf10982
Moses Mang <z1913099@turing.cs.niu.edu>
2022-04-29 21:00:52 -0500

    Add home button

--- html/pending.php
+++ html/pending.php
@@ -3,6 +3,10 @@ declare(strict_types = 1);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
+include '../lib/db.php';
+include '../lib/functions.php';
+session_start();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,13 +16,28 @@ error_reporting(E_ALL);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Checkout</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <style>
+    .home{
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        font-size: 25px;
+        margin: 20px 10px;
+    }
+    </style>
 </head>
 <body>
     <h1>Employee Page - Pending Orders</h1>    
     <h2>Pending Orders below:</h2>
+
+    <form method="POST">
+        <button class="button home" name="home">Home 
+            <i class="fa fa-home"></i>
+        </button>
+    </form>
 <?php
-include 'secrets.php';
-include 'library.php';
 
 //Connect to mariadb
 try {
