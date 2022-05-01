63b63d2abd6cceb9a9471c97a033e9d6cb35e53e
maricarmen p <maricarmen.paniagua@yahoo.com>
2022-05-01 05:26:22 -0500

    Moves connecting to mariadb to a function

--- lib/db.php
+++ lib/db.php
@@ -1,20 +1,19 @@
 <?php
-include '../config/secrets.php';
+    include '../config/secrets.php';
+    //Connect to mariadb function
+    function connectdb() {
+        global $dbname;
+        global $dbpassword;
+        global $dbusername;
+        try {
+            $dsn = "mysql:host=courses;dbname=$dbname";
+            $pdo = new PDO($dsn, $dbusername, $dbpassword);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        } 
+        catch(PDOException $error){  
+            die('    <p> Connection to database failed: ' . $error->getMessage() . "</p>\n </body></html>"); 
+        }
 
-//Connect to mariadb
-function connectdb() {
-    global $dbname;
-    global $dbpassword;
-    global $dbusername;
-    try {
-        $dsn = "mysql:host=courses;dbname=$dbname";
-        $pdo = new PDO($dsn, $dbusername, $dbpassword);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    } 
-    catch(PDOException $error){  
-        die('    <p> Connection to database failed: ' . $error->getMessage() . "</p>\n </body></html>"); 
+        return $pdo;
     }
-
-    return $pdo;
-}
 ?>
